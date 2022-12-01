c0470d72ba16dafa2c2bd97582fd46f77d0ce9ac
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2022-12-01 19:42:07 -0300

    Add day 1 solution

---
+++ Day1/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import Data.List (sortOn)
+import Data.List.Split (splitWhen)
+import Data.Ord (Down (Down), comparing)
+
+solve1 :: [[Int]] -> Int
+solve1 = maximum . map sum
+
+solve2 :: [[Int]] -> Int
+solve2 = sum . take 3 . sortOn Down . map sum
+
+parse :: String -> [[Int]]
+parse = map (map read) . splitWhen null . lines
+
+main :: IO ()
+main = do
+  input <- parse <$> getContents
+  print (solve1 input)
+  print (solve2 input)
